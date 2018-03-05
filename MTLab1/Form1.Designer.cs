cc8d847aadf2862c145c6e59eb55c97f3941084d
mistergad <audi-tore05@mail.ru>
2018-03-05 21:26:04 +0300

    lsb and jbc algorithms added

---
+++ MTLab1/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MTLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(782, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 256);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(516, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вставить сообщение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(414, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(368, 144);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Извлечь сообщение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(414, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 38);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод стеганографии";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(65, 15);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "LSB";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(165, 15);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(167, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Куттера-Джордана-Боссена";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 345);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ MTLab1/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MTLab1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        byte[] imageBin;
+        string fileName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Bmp файл|*.bmp";
+            openFileDialog.FilterIndex = 0;
+            DialogResult userClickedOK = openFileDialog.ShowDialog();
+            if (userClickedOK == DialogResult.OK)
+            {
+                fileName = openFileDialog.FileName;
+                if (File.Exists(fileName))
+                {
+                    image = new Bitmap(fileName);
+                    pictureBox1.Image = image;
+                    radioButton1.Checked = true;
+                    textBox1.Clear();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+                LSBInsert();
+            else
+                KJBInsert();
+        }
+
+        private void LSBInsert()
+        {
+            if (textBox1.Text.Length * 8 > 255)
+            {
+                MessageBox.Show("Длина сообщения не может превышать 30 символов");
+                return;
+            }
+            if (textBox1.Text.Length * 8 > (image.Width - 1) * (image.Height - 1))
+            {
+                MessageBox.Show("Размер изображения слишком мал для вставки текста");
+                return;
+            }
+            byte[] imageBinary = File.ReadAllBytes(fileName);
+            int startByte = Convert.ToInt32(imageBinary[10]);
+            BitArray message = new BitArray(Encoding.UTF8.GetBytes(textBox1.Text));
+            BitArray messageLength = new BitArray(new byte[] { Convert.ToByte(message.Length) });
+
+            for (int i = startByte, k = 0; i < startByte + 4; i++, k += 2)
+            {
+                BitArray temp = new BitArray(new byte[] { imageBinary[i] });
+                temp[0] = messageLength[k];
+                temp[1] = messageLength[k + 1];
+                temp.CopyTo(imageBinary, i);
+            }
+
+            for (int i = startByte + 4, k = 0; i < imageBinary.Length; i++, k += 2)
+            {
+                BitArray temp = new BitArray(new byte[] { imageBinary[i] });
+                if (k < message.Length)
+                    temp[0] = message[k];
+                if (k + 1 < message.Length)
+                    temp[1] = message[k + 1];
+                temp.CopyTo(imageBinary, i);
+            }
+            imageBin = imageBinary;
+        }
+
+        private void KJBInsert()
+        {
+            if (textBox1.Text.Length * 8 > 255)
+            {
+                MessageBox.Show("Длина сообщения не может превышать 30 символов");
+                return;
+            }
+            if (textBox1.Text.Length * 8 > (image.Width - 1) * (image.Height - 1))
+            {
+                MessageBox.Show("Размер изображения слишком мал для вставки текста");
+                return;
+            }
+
+            BitArray message = new BitArray(Encoding.UTF8.GetBytes(textBox1.Text));
+            BitArray messageLength = new BitArray(new byte[] { Convert.ToByte(message.Length) });
+
+            int x, y, k;
+            if (3 < image.Width && 3 < image.Height)
+            {
+                for (x = 3, y = 3, k = 0; k < 8; k++)
+                {
+                    int R = image.GetPixel(x, y).R;
+                    int G = image.GetPixel(x, y).G;
+                    int B = image.GetPixel(x, y).B;
+                    int Y = (int)(0.3 * R + 0.59 * G + 0.11 * B);
+                    int Bnew;
+                    if (messageLength[k] == true)
+                        Bnew = (int)(B + 0.25 * Y);
+                    else
+                        Bnew = (int)(B - 0.25 * Y);
+
+                    if (Bnew > 255)
+                        Bnew = 255;
+                    else if (Bnew < 0)
+                        Bnew = 0;
+
+                    image.SetPixel(x, y, Color.FromArgb(R, G, Bnew));
+                    x += 4;
+                    if (x + 4 >= image.Width)
+                    {
+                        x = 3;
+                        y += 4;
+                    }
+                }
+
+                for (k = 0; k < message.Length; k++)
+                {
+                    int R = image.GetPixel(x, y).R;
+                    int G = image.GetPixel(x, y).G;
+                    int B = image.GetPixel(x, y).B;
+                    int Y = (int)(0.3 * R + 0.59 * G + 0.11 * B);
+                    int Bnew;
+                    if (message[k] == true)
+                        Bnew = (int)(B + 0.25 * Y);
+                    else
+                        Bnew = (int)(B - 0.25 * Y);
+
+                    if (Bnew > 255)
+                        Bnew = 255;
+                    else if (Bnew < 0)
+                        Bnew = 0;
+                    image.SetPixel(x, y, Color.FromArgb(R, G, Bnew));
+                    x += 4;
+                    if (x + 4 >= image.Width)
+                    {
+                        x = 3;
+                        y += 4;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+                LSBExtract();
+            else
+                KJBExtract();
+        }
+
+        private void LSBExtract()
+        {
+            byte[] imageBinary = File.ReadAllBytes(fileName);
+            int startByte = Convert.ToInt32(imageBinary[10]);
+            BitArray messageLengthBin = new BitArray(8);
+            byte[] messageLengthByte = new byte[1];
+            for (int i = startByte, k = 0; i < startByte + 4; i++, k += 2)
+            {
+                BitArray temp = new BitArray(new byte[] { imageBinary[i] });
+                messageLengthBin[k] = temp[0];
+                messageLengthBin[k + 1] = temp[1];
+            }
+            messageLengthBin.CopyTo(messageLengthByte, 0);
+            int messageLength = Convert.ToInt32(messageLengthByte[0]);
+            BitArray messageBits = new BitArray(messageLength);
+
+            for (int i = startByte + 4, k = 0; k < messageLength; i++, k += 2)
+            {
+                BitArray temp = new BitArray(new byte[] { imageBinary[i] });
+                messageBits[k] = temp[0];
+                messageBits[k + 1] = temp[1];
+            }
+            byte[] messageByte = new byte[messageLength / 8];
+            messageBits.CopyTo(messageByte, 0);
+            textBox1.Text = Encoding.UTF8.GetString(messageByte);
+        }
+
+        private void KJBExtract()
+        {
+            BitArray messageLengthBin = new BitArray(8);
+            byte[] messageLengthByte = new byte[1];
+            int Bsum = 0;
+            int x, y, k;
+            if (3 < image.Width && 3 < image.Height)
+            {
+                for (x = 3, y = 3, k = 0; k < 8; k++)
+                {
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x, y + i).B;
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x, y - i).B;
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x + i, y).B;
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x - i, y).B;
+                    Bsum /= 12;
+
+                    if (image.GetPixel(x, y).B > Bsum)
+                        messageLengthBin[k] = true;
+                    else
+                        messageLengthBin[k] = false;
+
+                    x += 4;
+                    if (x + 4 >= image.Width)
+                    {
+                        x = 3;
+                        y += 4;
+                    }
+                }
+                messageLengthBin.CopyTo(messageLengthByte, 0);
+                int messageLength = Convert.ToInt32(messageLengthByte[0]);
+                BitArray messageBits = new BitArray(messageLength);
+                Bsum = 0;
+                for (k = 0; k < messageLength; k++)
+                {
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x, y + i).B;
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x, y - i).B;
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x + i, y).B;
+                    for (int i = 1; i <= 3; i++)
+                        Bsum += image.GetPixel(x - i, y).B;
+                    Bsum /= 12;
+
+                    if (image.GetPixel(x, y).B > Bsum)
+                        messageBits[k] = true;
+                    else
+                        messageBits[k] = false;
+
+                    x += 4;
+                    if (x + 4 >= image.Width)
+                    {
+                        x = 3;
+                        y += 4;
+                    }
+                }
+                byte[] messageByte = new byte[messageLength / 8];
+                messageBits.CopyTo(messageByte, 0);
+                textBox1.Text = Encoding.UTF8.GetString(messageByte);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Bmp файл|*.bmp";
+            saveFileDialog.FilterIndex = 0;
+            DialogResult userClicked = saveFileDialog.ShowDialog();
+            if (userClicked == DialogResult.OK)
+            {
+                string fileName2 = saveFileDialog.FileName;
+                if (radioButton1.Checked == true)
+                {
+                    File.WriteAllBytes(fileName2, imageBin);
+                }
+                else
+                {
+                    image.Save(fileName2);
+                }
+            }
+        }
+    }
+}
